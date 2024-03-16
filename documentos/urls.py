220a1c45706ff08b5ed6200aee354ada4143577a
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-03-16 09:29:05 -0500

    commit

--- documentos/urls.py
+++ documentos/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('documentosCarga', views.documentosCarga_view, name='documentosCarga_view'),
-    #path('documentosCarga/submit', views.documentosCarga_view, name='documentosCarga_view'),
+    path('documentosCarga/<int:session_pk>', views.documentosCarga_view, name='documentosCarga_view'),
     path('documentosCarga/<int:doc_pk>', views.documentoCarga_view, name='documentoCarga_view')
 ]
--- documentos/views.py
+++ documentos/views.py
@@ -5,8 +5,7 @@ from django.http import HttpResponse
 from django.core import serializers
 import json
 from django.views.decorators.csrf import csrf_exempt
-from .forms import FileUploadForm
-from .models import DocumentoCarga
+
 
 
 @csrf_exempt
@@ -15,7 +14,7 @@ def documentosCarga_template(request):
     return HttpResponse(template.render())
     
 @csrf_exempt
-def documentosCarga_view(request):
+def documentosCarga_view(request, session_pk):
     if request.method == 'GET':
         id = request.GET.get('id', None)
         if id:
@@ -31,13 +30,7 @@ def documentosCarga_view(request):
         documentoCarga_dto = ldc.create_documentoCarga(json.loads(request.body))
         documentoCarga = serializers.serialize('json', [documentoCarga_dto,])
         return HttpResponse(documentoCarga, 'application/json')
-        
     
-def lista_DocumentosCargados(request):
-    archivos = DocumentoCarga.objects.all()
-    return render(request, 'documentosCarga.html',{'archivos':archivos})
-
-
 
 @csrf_exempt
 def documentoCarga_view(request, doc_pk):
