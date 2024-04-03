dc4b4e1df7fa710bcf4e72be45f38823ab861e44
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-04-02 23:03:13 -0500

    cambios

--- documentos/static/documentosCarga.js
+++ documentos/static/documentosCarga.js
@@ -16,11 +16,10 @@ function editarDoc(docId, row){
     let docElem = document.getElementById("updateFile");
     docElem.addEventListener('change', function(event) {
         doc = event.target.files[0];
-        console
         if(doc){
             fetch(`${docId}`,{
                 method:"PUT",
-                body:JSON.stringify(doc)
+                body:doc
             })
             .then(response => response.json())
             .then(data => console.log(data[0]))
