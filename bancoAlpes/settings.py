5b7275379d2a5abec8d21950f60b233a6be2b7fc
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-04 04:53:29 -0500

    http https

--- bancoAlpes/settings.py
+++ bancoAlpes/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'bootstrap5',
     'bancoAlpes',
     'landing',
@@ -89,8 +90,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
-
+CORS_ALLOW_ALL_ORIGINS = True
 ROOT_URLCONF = 'bancoAlpes.urls'
 
 TEMPLATES = [
