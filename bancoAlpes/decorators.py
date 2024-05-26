3146ae32e6bf3d34bd0b9b882e562bf1200d895d
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 16:20:18 -0500

    loginpage

--- bancoAlpes/decorators.py
+++ bancoAlpes/decorators.py
@@ -8,12 +8,12 @@ from django.views.decorators.csrf import csrf_exempt
 def token_required(view_func):
     def _wrapped_view_func(request, *args, **kwargs):
         if not request.session["user_token"]:
-            return render(request, "loginPageForm.html")    
+            return render(request, "loginPage.html")    
         else:
             response = requests.get("http://35.190.51.156:8082/user/is_authenticated?token=" + request.session["user_token"])
             
             if "error" in response.text:
-                return render(request, "loginPageForm.html")
+                return render(request, "loginPage.html")
             else:
                 return redirect("http://34.110.196.225/indexdocumentos/?token=" + request.session["user_token"])
         return view_func(request, *args, **kwargs)
