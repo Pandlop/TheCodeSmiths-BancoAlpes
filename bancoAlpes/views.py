9fb5e22f1f2a93cc95144005656ae68d05005b30
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 12:55:11 -0500

    aaaa

--- bancoAlpes/views.py
+++ bancoAlpes/views.py
@@ -149,7 +149,7 @@ def submit_login_info(request):
     if response.status_code == 200:
         # Extraer el token de la respuesta JSON
         print(response)
-        request.session["user_token"] = response["access_token"]  # Asegúrate de extraer solo el token
+        # request.session["user_token"] = response.get(["access_token"]  # Asegúrate de extraer solo el token
 
         return HttpResponse(request.session["user_token"])
     else:
