d916940f463055d95c64d0f5da076999c31e3bf2
JuanCamilo Lopez <jc.lopezc1@uniandes.edu.co>
2024-05-26 21:59:48 -0500

    solcitidues

--- landing/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from . import views
-urlpatterns = [
-
-    path('', views.landingPage, name='landingPage'),
-    path('landingPage', views.landingPage, name='landingPage'),
-
-]
